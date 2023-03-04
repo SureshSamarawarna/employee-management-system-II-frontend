a51c0b37de9d3663484c10cbe9a405fe35f701cb
SureshSamarawarna <suresh.asan8@gmail.com>
2023-03-04 23:48:37 +0530

    completed AddEmployee page styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css"
+import AddEmployee from "./components/AddEmployee"
 import Navbar from "./components/Navbar"
 
 export default function App() {
   return (
+  <>
     <Navbar></Navbar>
-  )
+    <AddEmployee></AddEmployee>
+  </>
+  );
 }
